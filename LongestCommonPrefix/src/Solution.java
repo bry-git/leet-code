ca9a9834baa0585f9ee804b55cea1fc6d86fadd5
bryson crandall <bryson.crandall@yahoo.com>
2022-08-12 16:28:12 -0700

    adding missing solution to product except self

---
+++ LongestCommonPrefix/src/Solution.java
@@ -0,0 +1,17 @@
+
+
+public class Solution {
+    public static String getLongestCommonPrefix(String[] inputs) {
+        String result = "";
+
+        //step through word
+        for (int i = 0; i < inputs.length - 1; i++) {
+
+            //step through chars in word
+            for(int j = 0; j < inputs[i].split("").length; j++){
+
+            }
+        }
+        return result;
+    }
+}
--- productExceptSelf/src/Solution.java
+++ productExceptSelf/src/Solution.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.Arrays;
+
+public class Solution {
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i <= nums.length -1; i++) {
+
+            if(nums[i] == 0) throw new RuntimeException("cannot multiply by zero");
+
+            int curr = nums[i];
+            nums[i] = 1;
+            int productExceptSelf = 1;
+            for(int j : nums) {
+                productExceptSelf*=j;
+            }
+            result[i] = productExceptSelf;
+            nums[i] = curr;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3}))); // [6, 3, 2]
+        System.out.println(Arrays.toString(productExceptSelf(new int[] {-1, 1, -1, 1, -1})));
+    }
 }
